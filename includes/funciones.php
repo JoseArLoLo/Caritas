bfd61b5d9be19b80109337c66421bdf2dcff7e5d
JoseArLoLo <josearturolopezlopez2@gmail.com>
2024-04-15 19:15:04 -0700

    Carga de bases para administracion

--- includes/funciones.php
+++ includes/funciones.php
@@ -4,4 +4,30 @@ require 'app.php';
 function incluirTemplate( string $nombre, $donar = false, $eventos = false) {
     include TEMPLATES_URL . "/$nombre.php";
 }
+//Para condiciones if de ser necesario
+function estaAutenticado() {
+    session_start();
+    if($_SESSION['login']) {
+        return true;
+    }
+    return false;
+}
+//Para redireccionar en caso de que no exista
+function logueado() {
+    session_start();
+    if(!$_SESSION['login']) {
+        header('Location: /');
+    }
+}
+//Escapa / Sanitizar el HTML
+function s($html): string {
+    $s = htmlspecialchars($html);
+    return $s;
+}
+function debuguear($variable) {
+    echo "<pre>";
+    var_dump($variable);
+    echo "</pre>";
+    exit;
+}
 ?>
---
+++ login/index.php
@@ -0,0 +1,27 @@
+<?php
+require '../includes/funciones.php';
+incluirTemplate('header');
+?>
+<main class="contenedor seccion">
+    <h1 class="titulo-center-rojo">Iniciar Sesión</h1>
+
+    <?php foreach ($errores as $error) : ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+    <form method="POST" class="formulario">
+        <fieldset>
+            <legend>Usuario y contraseña</legend>
+
+            <label for="user">usuario</label>
+            <input type="text" name="user" placeholder="Tu usuario" id="user">
+
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="Tu Password" id="password">
+        </fieldset>
+
+        <input type="submit" value="Iniciar Sesión" class="boton-verde">
+    </form>
+</main>
+<?php incluirTemplate('footer'); ?>
